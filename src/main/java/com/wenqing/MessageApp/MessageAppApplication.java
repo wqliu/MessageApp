cb4f5b7faf9e777b824d3d8f3bf7e0f1cbf0975f
wenqing <woshiliuwenqing@gmail.com>
2020-03-25 12:06:11 -0500

    initial commit

---
+++ src/main/java/com/wenqing/MessageApp/MessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.wenqing.MessageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wenqing/MessageApp/MessageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenqing.MessageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
